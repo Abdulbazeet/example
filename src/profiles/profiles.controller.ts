edcc91641c15f9a9b28377c45cc40c7979900413
Abdulbazeet <abdulbazeetbalogun4@gmail.com>
2026-09-14 19:33:13 +0100

    Refactor code structure for improved readability and maintainability

--- src/profiles/profiles.controller.ts
+++ src/profiles/profiles.controller.ts
@@ -1,41 +1,47 @@
 import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Post, Put, Query } from '@nestjs/common';
 import { CreateProfileDto } from './dto/create_profile.dto.js';
 import { UpdateProfileDto } from './dto/update-profile.dto.js';
+import { ProfilesService } from './profiles.service.js';
 
 @Controller('profiles')
 export class ProfilesController {
 
-  costructor() {}
+  constructor(private profileService: ProfilesService) {}
 
     //find all profiles
     @Get()
-    findAllProfiles(@Query('age') age: number){
-        return [{age}]
+    findAllProfiles(){
+        return this.profileService.findAllProfile()
 
     }
 
     //find one profile by id
+    // GET /profiles/:id
     @Get(':id')
-    findOneById(@Param('id') id:String){
-        return {id}
+    findOneById(@Param('id') id: string){
+        return this.profileService.findOneProfileById(id)
     }
 
     //create-profile
+    // POST /profiles
     @Post()
   createProfile(@Body() createProfileDto: CreateProfileDto){
-    return {name : createProfileDto.name, description: createProfileDto.description}
+    return this.profileService.createProfile(createProfileDto)
   }  
 
   //update-profile
+  // PUT /profiles/:id
   @Put(':id')
   updateProfile(@Param('id') id: string, @Body() updateProfileDto: UpdateProfileDto){
-    return {id, ...updateProfileDto}
+    return this.profileService.updateProfile(id, updateProfileDto)
   }
 
   //delete-profile
+  // DELETE /profiles/:id
   @Delete(':id')
   @HttpCode(HttpStatus.NO_CONTENT)
   deleteProfile(@Param('id') id: string){
+    return this.profileService.deleteProfile(id)
     
   }
 }
--- src/profiles/profiles.service.ts
+++ src/profiles/profiles.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { randomUUID } from 'crypto';
+import { CreateProfileDto } from './dto/create_profile.dto.js';
+import { UpdateProfileDto } from './dto/update-profile.dto.js';
+import { profile } from 'console';
 
 @Injectable()
 export class ProfilesService {
@@ -13,4 +16,35 @@ export class ProfilesService {
     findAllProfile(){
         return this.profiles
     }
+
+    findOneProfileById(id: string){
+        return this.profiles.find((profile) => profile.id === id)
+    }
+
+
+    
+    createProfile(createProfileDto: CreateProfileDto){
+
+        const newProfile = {id: randomUUID(), ...createProfileDto}
+        this.profiles.push(newProfile)
+        return newProfile
+       
+    }
+
+    updateProfile(id: string, updateProfileDto: UpdateProfileDto){
+        const profileIndex = this.profiles.findIndex((profile) => profile.id === id)
+        if(profileIndex === -1){
+            return null
+        }
+        const updatedProfile = {...this.profiles[profileIndex], ...updateProfileDto}
+        this.profiles[profileIndex] = updatedProfile
+        return updatedProfile
+    }
+    deleteProfile(id: string):void{
+       const matchingIndex = this.profiles.findIndex((profile) => profile.id ===id)
+      if(matchingIndex > -1){
+        this.profiles.splice(matchingIndex,1)
+      }
+    }
+
 }
